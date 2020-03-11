7add00c9efb595662ca8b8ccf0df6f0e637407f9
SleepingNext <veroskevin8@gmail.com>
2020-03-11 12:00:45 +0700

    Integrate with JWT

--- main.go
+++ main.go
@@ -3,20 +3,29 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	"log"
 
 	"github.com/G0tYou/user-service/database"
 	"github.com/G0tYou/user-service/handler"
 	userPB "github.com/G0tYou/user-service/proto"
 	"github.com/G0tYou/user-service/repository/postgres"
+	authPB "github.com/SleepingNext/auth-service/proto"
 	_ "github.com/lib/pq"
 	"github.com/micro/go-micro"
 )
+
+var methodsWithoutAuth = map[string]bool{"User.Show": true, "User.ShowUserByUsername": true, "User.StoreUser": true, }
 
 func main() {
 	// Setup the micro instance
 	s := micro.NewService(
 		micro.Name("com.ta04.srv.user"),
+		micro.WrapHandler(AuthWrapper),
 	)
 	// Initialize the service
 	s.Init()
@@ -45,5 +54,33 @@ func main() {
 	err = s.Run()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, res interface{}) error {
+		method := req.Method()
+		if _, ok := methodsWithoutAuth[method]; ok {
+			return fn(ctx, req, res)
+		}
+
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in the request")
+		}
+
+		token := meta["Token"]
+		log.Println("authenticating with token: ", token)
+
+		// Validate the token
+		authClient := authPB.NewAuthServiceClient("go.micro.srv.auth", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &authPB.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, res)
+		return err
 	}
 }
